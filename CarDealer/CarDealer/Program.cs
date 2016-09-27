aca5c63e9500c9f08ad1e7dd58d766347c440895
Frank <towettf@yahoo.com>
2016-09-27 14:48:00 -0500

    CarDealer and Rectangle classes
    Creating a car class for car dealer and reactangle class with objects.

---
+++ CarDealer/CarDealer/Program.cs
@@ -0,0 +1,42 @@
+﻿//
+//Developer: Franklin Towett
+//Project: CarDealer Console Application - Using Classes and Objects
+//Class: IN2017
+//Instructor: R Ray.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an object from the car class
+            Car myCar = new Car();
+
+            myCar.Make = "Mercedes Benz";
+            myCar.Model = "E Class";
+            myCar.Year = 2016;
+            myCar.Color = "Black";
+
+            Console.WriteLine("My car is a {0} {1} {2} and the color is {3} .", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            Console.ReadLine();
+        }
+    }
+    class Car
+    {
+        public string Make { get; set;}
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+    }
+}
---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,65 @@
+﻿//
+//Developer: Franklin Towett
+//Project: Lesson02 - Using Classes and Objects
+//Class: IN2017
+//Instructor: R Ray.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Length = 20.0;
+            rect.Width = 10.0;
+            double area = rect.GetArea();
+            Console.WriteLine("Area of the Rectangle is: {0}.", area );
+            Console.ReadLine();
+        }
+    }
+    class Rectangle
+    {
+        private double length;
+
+        private double width;
+            public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value > 0.0)
+
+                    length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0.0)
+
+                    width = value;
+            }
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+    }
+}
